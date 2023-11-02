68aeb30b44f3f5cec9c82b5d284ddf3279969ca0
PinkSquacht <Lloyd.bowar@gmail.com>
2023-11-01 21:09:27 -0700

    finished my homework

--- day1homework.sql
+++ day1homework.sql
@@ -2,22 +2,52 @@
 
 -- 1. How many actors are there with the last name ‘Wahlberg’?
 
+SELECT COUNT(last_name)
+FROM actor
+WHERE last_name = 'Wahlberg';
+
 -- 2. How many payments were made between $3.99 and $5.99?
+
+SELECT COUNT(amount)
+From payment
+WHERE amount BETWEEN 3.99 AND 5.99;
 
 -- 3. What film does the store have the most of? (search in inventory)
 
 -- 4. How many customers have the last name ‘William’?
-
+SELECT COUNT(last_name)
+FROM customer
+WHERE last_name = 'Williams';
 -- 5. What store employee (get the id) sold the most rentals?
-
+SELECT staff_id, COUNT(rental_id)
+FROM rental
+GROUP BY staff_id
+ORDER BY COUNT(rental_id) DESC
+LIMIT 1;
 -- 6. How many different district names are there?
+SELECT COUNT(DISTINCT district)
+FROM address;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
-
+SELECT film_id COUNT(actor_id)
+from film_actor
+GROUP BY film_id
+ORDER BY COUNT(actor_id) DESC
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
-
+SELECT COUNT(last_name)
+FROM customer
+WHERE last_name LIKE '%es'
+AND store_id = 1;
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 -- with ids between 380 and 430? (use group by and having > 250)
+SELECT amount, COUNT(rental_id)
+FROM payment
+WHERE customer_id BETWEEN 380 AND 430
+GROUP BY amount
+HAVING COUNT(rental_id) > 250;
+
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most
 -- movies total?
+SELECT COUNT(DISTINCT rating)
+FROM film;
